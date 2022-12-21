4b7c0389f477c69e83994c6e2546cd3efb1da304
ryokosuge <ryokosuge@users.noreply.github.com>
2022-12-21 22:30:54 +0900

    Shopify App Bridge の導入 (#7)
    * chore:	yarn add @shopify/app-bridge @shopify/app-bridge-utils @shopify/app-bridge-react
    
    * feat:	Provider

--- web/app/root.tsx
+++ web/app/root.tsx
@@ -1,4 +1,10 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { FC } from "react";
+import type {
+  HeadersFunction,
+  LoaderArgs,
+  MetaFunction,
+} from "@remix-run/node";
+import { json } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -6,7 +12,52 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
+
+import type { ProviderProps } from "@shopify/app-bridge-react";
+import { Provider } from "@shopify/app-bridge-react";
+import { shopifyRequestMiddleware } from "./middleware/shopifyRequestMiddleware.server";
+
+export const loader = async ({ request }: LoaderArgs) => {
+  const payload = await shopifyRequestMiddleware({ request });
+  if (!payload.hasAccessToken) {
+    return json(
+      {
+        myshopifyDomain: payload.shop,
+        host: payload.host,
+        apiKey: process.env.SHOPIFY_API_KEY,
+      },
+      {
+        status: 200,
+        headers: {
+          "Content-Security-Policy": `frame-ancestors https://${payload.shop} https://admin.shopify.com`,
+        },
+      },
+    );
+  }
+
+  return json(
+    {
+      myshopifyDomain: payload.shop,
+      host: payload.host,
+      apiKey: process.env.SHOPIFY_API_KEY,
+    },
+    {
+      status: 200,
+      headers: {
+        "Content-Security-Policy": `frame-ancestors https://${payload.shop} https://admin.shopify.com`,
+      },
+    },
+  );
+};
+
+export const headers: HeadersFunction = ({ loaderHeaders, actionHeaders }) => ({
+  "Content-Security-Policy":
+    loaderHeaders.get("Content-Security-Policy") ??
+    actionHeaders.get("Content-Security-Policy") ??
+    "",
+});
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
@@ -14,7 +65,14 @@ export const meta: MetaFunction = () => ({
   viewport: "width=device-width,initial-scale=1",
 });
 
-export default function App() {
+const App: FC = () => {
+  const data = useLoaderData<typeof loader>();
+  const config: ProviderProps["config"] = {
+    apiKey: data.apiKey,
+    host: data.host,
+    forceRedirect: true,
+  };
+
   return (
     <html lang="en">
       <head>
@@ -22,11 +80,15 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <Outlet />
+        <Provider config={config}>
+          <Outlet />
+        </Provider>
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
       </body>
     </html>
   );
-}
+};
+
+export default App;
